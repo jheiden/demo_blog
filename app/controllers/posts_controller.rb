c95fad58f078328bb76b67f3c34d2fc52249c611
Johan Heidenberg <jheidenberg@outlook.com>
2019-01-06 13:20:16 +0100

    added basic methods to controller => added new view template 36m 13s

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,29 @@ class PostsController < ApplicationController
 
   end
 
+  def new
+    @post = Post.new
   
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save # ".. if record was created => e.g, passed validation.."
+      redirect_to @post # redirect to the <post> just created
+    else
+      render 'new' # render the <new> template
+    end
+    
+  end
+
+  private
+
+  def post_params
+    # "Security feature" of rails that allows us to declare what type of input the form will accept when creating record.
+    # set to true if passes validation.
+    params.require(:post).permit(:title, :content) 
+  end
+
+
 end
