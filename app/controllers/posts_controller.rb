e4d917536c60cd5fcecc13244dda23bccf290b99
Johan Heidenberg <jheidenberg@outlook.com>
2019-01-06 16:28:19 +0100

    added edit / delete buttons to the show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,16 +6,14 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
-  
   end
 
   def create
     @post = Post.new(post_params)
-
     if @post.save # ".. if record was created => e.g, passed validation.."
       redirect_to @post # redirect to the <post> just created
     else
-      render 'new' # render the <new> template
+      render 'new' # render the <new> template again
     end
   end
     
@@ -25,8 +23,6 @@ class PostsController < ApplicationController
  
   end
 
-
-
   private
 
   def post_params
@@ -34,6 +30,26 @@ class PostsController < ApplicationController
     # set to true if passes validation.
     params.require(:post).permit(:title, :content) 
   end
+
+  # whilst update is the actual change to records
+  def update
+    @post = Post.find(params[:id])
+    # "..if passed our security method declared above"
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit' # else render the <edit> template again.
+    end
+
+  end
+
+  # edit is the view that we will render allowing user to make changes
+  def edit
+    @post = Post.find(params[:id])
+
+  end
+  
+
 
 
 end
