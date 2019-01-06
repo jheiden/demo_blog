c2b8547ebc8af09a8d10db9385f8a11c12d75108
Johan Heidenberg <jheidenberg@outlook.com>
2019-01-06 18:55:03 +0100

    edit now works. Changed view structure through adding a _form partial and then i render form when needed from the new.html.erb and the edit.html.erb views.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,25 @@ class PostsController < ApplicationController
  
   end
 
+# whilst update is the actual change to records
+def update
+  @post = Post.find(params[:id])
+  # "..if passed our security method declared above"
+  if @post.update(post_params)
+    redirect_to @post
+  else
+    render 'edit' # else render the <edit> template again.
+  end
+  
+end
+
+# edit is the view that we will render allowing user to make changes
+def edit
+  @post = Post.find(params[:id])
+end
+
+
+# private declaration must be at bottom of page o/w other methods wont be accessible..
   private
 
   def post_params
@@ -30,26 +49,5 @@ class PostsController < ApplicationController
     # set to true if passes validation.
     params.require(:post).permit(:title, :content) 
   end
-
-  # whilst update is the actual change to records
-  def update
-    @post = Post.find(params[:id])
-    # "..if passed our security method declared above"
-    if @post.update(post_params)
-      redirect_to @post
-    else
-      render 'edit' # else render the <edit> template again.
-    end
-
-  end
-
-  # edit is the view that we will render allowing user to make changes
-  def edit
-    @post = Post.find(params[:id])
-
-  end
-  
-
-
 
 end
