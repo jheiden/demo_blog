63b9d6732379c91509db049fbbe496864e4f68cb
Johan Heidenberg <jheidenberg@outlook.com>
2019-01-06 19:14:05 +0100

    delete implemented => everything works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,6 +40,12 @@ def edit
   @post = Post.find(params[:id])
 end
 
+def destroy
+ @post = Post.find(params[:id])
+ @post.destroy
+ 
+ redirect_to posts_path
+end
 
 # private declaration must be at bottom of page o/w other methods wont be accessible..
   private
